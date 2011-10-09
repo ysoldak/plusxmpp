434269184d566ccc1f6bf43c47c5aaacc0ccc8ba
Yurii Soldak <ysoldak@gmail.com>
2011-10-09 23:50:00 +0200

    Renamed to plusxmpp. Extracts posts from friends and sends to the subscribed user.

--- plusxmpp.py
+++ plusxmpp.py
@@ -1,86 +1,56 @@
 import logging
-import re
+import time
 
-from django.utils import simplejson
-
-from google.appengine.api import urlfetch
 from google.appengine.api import xmpp
 from google.appengine.ext import webapp
+from google.appengine.ext import db
+from google.appengine.api import taskqueue
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-#my plus id 118056070123837000922
-
-commas = re.compile(',,',re.M)
+import core
+import tasks
 
 class XMPPHandler(webapp.RequestHandler):
 	def post(self):
 		message = xmpp.Message(self.request.POST)
-		logging.info(message.body)
-		if message.body[0:2].lower() == 'f:' or message.body[0:2].lower() == 'f ': # Friends user follows
-			message.reply(self.doFetchFriends(message.body[2:]), raw_xml=False)
-		else if message.body[0:2].lower() == 'l:' or message.body[0:2].lower() == 'l ': # latest posts
-			message.reply(self.doFetchFriends(message.body[2:]), raw_xml=False)
-	
-	def doFetchFriends(self, plus_id):
-		url = 'https://plus.google.com/_/socialgraph/lookup/visible/?o=%5Bnull%2Cnull%2C%22'+plus_id+'%22%5D&_reqid=5582440&rt=j'
-		logging.info(url)
-		result = urlfetch.fetch(url)
 
-		if result.status_code != 200:
-			logging.error('Unexpected status code: %d' % result.status_code)
-			return "Can't fetch friends list."
+		logging.debug("jid:"+message.sender)
+		logging.debug("body:"+message.body)
 
-		received_content = result.content
-		txt = received_content[5:]
-		txt = commas.sub(',null,',txt)
-		txt = commas.sub(',null,',txt)
-		txt = txt.replace('[,','[null,')
-		txt = txt.replace(',]',',null]')
+		if message.body[0:5].lower() == 'plus ':
+			core.setUser(message.sender, message.body[5:])
+			message.reply("Subscribed!")
+			return
 
-		json_decoder = simplejson.decoder.JSONDecoder()
-		try:
-			decoded_json = json_decoder.decode(txt)
-		except ValueError:
-			logging.error("Can't parse data")
-			return "Can't fetch friends list."
-		#else:
-		#	logging.info('Decoded JSON: %s' % str(decoded_json))
-		
-		friends = decoded_json[0][0][2]
-		friends_count = len(friends)
+		user = core.getUser(message.sender)
+		if user is None:
+			message.reply("Welcome to PlusXMPP service.\nPlease, send me your +id in the form:\nplus 1234567890")
+			return
 
-		logging.debug('Number of friends ('+plus_id+'): ' + str(friends_count))
+		if message.body.lower() == 'f': # friends user follows
+			message.reply(core.getFriends(user.plus_id), raw_xml=False)
+			return
 
-		if friends_count == 0:
-			return "Can't fetch list of friends. Public access restricted?"
+		if message.body.lower() == 'l': # latest (cached) posts
+			posts = core.getLatest(user.plus_id, -1)
+			if posts is None or posts == '':
+				posts = "No new posts found."
+			message.reply(posts, raw_xml=False)
+			return
 
-		count = 0
-		respond = ""
+		if message.body.lower() == 't': # test
+			message.reply(str(time.time()), raw_xml=False)
+			return
 
-		for friend in friends:
-	
-			count = count + 1
-			if count > 10:
-				break
-			
-			#logging.info(str(friend[2]))
+class TaskHandler(webapp.RequestHandler):
 
-			respond += friend[2][0] + ' (https://plus.google.com/' + friend[0][2] + '/about)\n'
-			#respond += '<a href="https://plus.google.com/'+friend[0][2]+'/about">'+friend[2][0]+ '</a><br/>'
+	def get(self): # touched by cron
+		taskqueue.add(url='/fetch/posts', params={})
 
-		if friends_count > 10:
-			respond += '... and ' + str((friends_count - 10)) + ' more.'
-		
-		return respond #'<body>' + respond + '</body>'
+	def post(self): # touched by task queue
+		tasks.taskFetchPosts()
 
-	# def get(self):
-	#     user_address = 'ysoldak@gmail.com'
-	#     #chat_message_sent = False
-	#     msg = "Warmup"
-	#     status_code = xmpp.send_message(user_address, msg)
-	#     #chat_message_sent = (status_code == xmpp.NO_ERROR)
-
-application = webapp.WSGIApplication([('/_ah/xmpp/message/chat/', XMPPHandler),('/ping', XMPPHandler)],debug=True)
+application = webapp.WSGIApplication([('/_ah/xmpp/message/chat/', XMPPHandler),('/fetch/posts', TaskHandler)],debug=True)
 
 def main():
 	run_wsgi_app(application)
