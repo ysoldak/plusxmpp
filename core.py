2f93f38783a64a882e6fa54969df369e76661a69
Yurii Soldak <ysoldak@gmail.com>
2011-10-13 22:00:28 +0200

    Less logging, better fetch cycle constants

--- core.py
+++ core.py
@@ -5,9 +5,12 @@ from google.appengine.api import memcache
 import datamodel as dm
 import fetch
 
-CYCLE = 60 * 60
+# fetcher constants, must be backed by cron.yaml (set it to fire each MIN_CYCLE secs)
+MIN_CYCLE   = 60 * 10               # most active streams
+MAX_CYCLE   = 60 * 60 * 24          # most idle streams and friends list
+CYCLE_RATIO = MAX_CYCLE / MIN_CYCLE
+
 POST_LIMIT = 10
-MAX_SKIP_CYCLES = 24
 
 def init(jid, plus_id):
 	dm.set_user(jid, plus_id)
@@ -19,7 +22,7 @@ def get_friends(plus_id):
 	data = memcache.get("friends_" + plus_id)
 	if data is None:
 		data = fetch.friends(plus_id)
-		memcache.set("friends_"+plus_id, data, MAX_SKIP_CYCLES * CYCLE)
+		memcache.set("friends_"+plus_id, data, MAX_CYCLE)
 	return data
 
 
@@ -38,37 +41,39 @@ def get_posts(plus_id, timestamp):
 	posts = memcache.get("posts_" + plus_id)
 	if posts is None: # falls here only once during the fetch cycle
 		status = get_status(plus_id)
-		if status['ts'] is not None and (status['ts'] + CYCLE * status['freq'] - 60) > timestamp: # "-60" in case GAE doesn't run cron sharp at time
+		if status['ts'] is not None and (status['ts'] + MIN_CYCLE * status['freq'] - 60) > timestamp: # "-60" in case GAE doesn't run cron sharp at time
 			return ''
-		oldest = timestamp - status['freq'] * CYCLE + 1 # "+1" to ensure we don't harvest any already processed post
+		oldest = timestamp - status['freq'] * MIN_CYCLE + 1 # "+1" to ensure we don't harvest any already processed post
 		posts = fetch.posts(plus_id, oldest, POST_LIMIT)
 		status = update_status(plus_id, posts != '', timestamp)
-		memcache.set('posts_' + plus_id, posts, timestamp + CYCLE - 2) # "-2" to ensure the cache is expired before next fetch cycle
+		memcache.set('posts_' + plus_id, posts, timestamp + MIN_CYCLE - 2) # "-2" to ensure the cache is expired before next fetch cycle
 	return posts
 
 def get_status(plus_id):
 	status = memcache.get("status_" + plus_id)
 	if status is None:
 		status = {'ts':None, 'freq':1}
-	logging.debug("Get status for '" + plus_id + "': " + str(status))
+	#logging.debug("Get status for '" + plus_id + "': " + str(status))
 	return status
 
 def update_status(plus_id, has_new, timestamp):
 	status = get_status(plus_id)
 	
 	freq = status['freq']
+	old_freq = freq
 	
 	if has_new:
 		freq = 1 # fetch plus account posts on next cycle if harvested at least one post during this cycle
 	else:
 		freq *= 2 # otherwise postpone the fetch gradually to maximum (once a day for now)
-		if freq > MAX_SKIP_CYCLES:
-			freq = MAX_SKIP_CYCLES
+		if freq > CYCLE_RATIO:
+			freq = CYCLE_RATIO
 	
 	status['ts'] = timestamp
 	status['freq'] = freq
 	
-	logging.debug("Set status for '" + plus_id + "': " + str(status))
+	if old_freq != freq:
+		logging.debug("Frequency '" + plus_id + "': " + str(old_freq) + "->" + str(freq))
 	
 	memcache.set("status_" + plus_id, status)
 	
